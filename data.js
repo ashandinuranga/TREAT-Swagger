3977b0babc3cc83a1d27eb244c5c3146a3dd5415
ashan_ <ashan.dinuranga@vitalhub.com>
2026-09-24 12:31:35 +0530

    update server.js

--- data.js
+++ data.js
@@ -6,9 +6,9 @@ const users = [
 ];
 
 const hospitals = [
-  { idx: "H001", name: "Negombo General Hospital", city: "Negombo" },
-  { idx: "H002", name: "Colombo National Hospital", city: "Colombo" },
-  { idx: "H003", name: "Kandy Teaching Hospital", city: "Kandy" }
+  { id: "H001", name: "Negombo General Hospital", city: "Negombo" },
+  { id: "H002", name: "Colombo National Hospital", city: "Colombo" },
+  { id: "H003", name: "Kandy Teaching Hospital", city: "Kandy" }
 ];
 
 const patients = [
